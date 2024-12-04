492379c7e7bbe05348f7ec7198b2f1549abf39e8
Shinichi Haramizu <shinichi@haramizu.jp>
2024-12-04 10:40:01 +0900

    add carousel

--- examples/tailwind-shadcn-pages/src/pages/index.tsx
+++ examples/tailwind-shadcn-pages/src/pages/index.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   return (
-    <div className="m-10">
+    <div>
       <main>
-        <Button>Click me</Button>
+        <div className="m-20">
+          <Carousel>
+            <CarouselContent>
+              <CarouselItem>Item 1</CarouselItem>
+              <CarouselItem>Item 2</CarouselItem>
+              <CarouselItem>Item 3</CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+        <div className="m-10">
+          <Button>Click me</Button>
+        </div>
       </main>
     </div>
   );
